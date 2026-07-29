ad8e49c115889030a7881c32b4733448b3c1bbde
Zenox625 <adamdhirech2009@gmail.com>
2026-07-29 19:58:48 -0400

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import UniversePreviewClient from "@/components/universe/UniversePreviewClient";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return <UniversePreviewClient />;
 }
